019564bb30ca9bb69638e982eacbf31ee932c6d5
selly361 <seikou615@gmail.com>
2024-06-02 18:46:16 +0100

    Update: Export components `Main`, `Footer`, `CategoryButtons`

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,1 +1,4 @@
 export { default as Header } from './Header'
+export { default as Main } from './Main'
+export { default as CategoryButtons } from './CategoryButtons'
+export { default as Footer } from './Footer'
