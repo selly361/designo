6eed25c5c670f50cd616456297fc6b169e6701e9
selly361 <seikou615@gmail.com>
2024-06-02 18:39:17 +0100

    Add: `categoryRoutes` data

---
+++ src/Constants/categoryRoutes.ts
@@ -0,0 +1,16 @@
+export const categoryRoutes = [
+    {
+        name: 'WEB DESIGN',
+        route: 'web-design'
+    },
+
+    {
+        name: 'APP DESIGN',
+        route: 'app-design'
+    },
+
+    {
+        name: 'GRAPHIC DESIGN',
+        route: 'graphic-design'
+    }
+]
