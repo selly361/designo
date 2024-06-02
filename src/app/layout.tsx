ccba89476d0a88136eefadb6a047de356de9db67
selly361 <seikou615@gmail.com>
2024-06-02 18:43:02 +0100

    Update: refactor layout, remove unnecessary type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
 import Head from 'next/head'
 import { Jost } from 'next/font/google'
 import '@/Styles/globals.scss'
+import { Footer, Header, Main } from '@/Components'
+import { PropsWithChildren } from 'react'
 
 const jost = Jost({ subsets: ['latin'] })
 
-interface Props {
-	children: React.ReactNode
-}
 
-export default function RootLayout({ children }: Readonly<Props>) {
+export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
 	return (
 		<html lang='en'>
 			<Head>
@@ -16,7 +15,9 @@ export default function RootLayout({ children }: Readonly<Props>) {
 				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 			</Head>
 			<body className={jost.className}>
-				<main>{children}</main>
+				<Header />
+				<Main>{children}</Main>
+				<Footer />
 			</body>
 		</html>
 	)
